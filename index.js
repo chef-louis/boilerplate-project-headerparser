8b2053dfe5488c75f034292135447ed0e51c6afd
louis.code <louis.code88@gmail.com>
2023-08-24 16:28:28 -0600

    feat: add header parsing

--- index.js
+++ index.js
@@ -20,11 +20,15 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  res.json({
+    ipaddress: req.header("X-Forwarded-For"),
+    language: req.header('Accept-Language'),
+    software: req.header('User-Agent')
+  });
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 33333, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
